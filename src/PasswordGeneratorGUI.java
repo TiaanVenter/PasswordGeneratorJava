b1fcc368bdc609f32afea8e6a0fefce6e5fc52c8
TiaanVenter <cplkane@gmail.com>
2026-01-20 16:49:44 +0200

    Completed GUI

--- src/PasswordGeneratorGUI.java
+++ src/PasswordGeneratorGUI.java
@@ -32,7 +32,7 @@ public class PasswordGeneratorGUI extends JFrame {
         JLabel titleLabel = new JLabel("Password Generator");
 
         // increase font size, make it bold
-        titleLabel.setFont(new Font("Serif", Font.BOLD, 30));
+        titleLabel.setFont(new Font("Serif", Font.BOLD, 36));
 
         // center the text to the screen
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -44,5 +44,62 @@ public class PasswordGeneratorGUI extends JFrame {
         add(titleLabel);
 
         // create result text area
+        JTextArea passwordOutput = new JTextArea();
+
+        // prevent editing the text area
+        passwordOutput.setEditable(false);
+        passwordOutput.setFont(new Font("Serif", Font.BOLD, 30));
+
+        // add scrollability in case output is too big
+        JScrollPane passwordOutputPane = new JScrollPane(passwordOutput);
+        passwordOutputPane.setBounds(25, 97, 479, 70);
+
+        // set border around text area
+        passwordOutputPane.setBorder(BorderFactory.createBevelBorder(0, Color.BLACK, Color.YELLOW));
+        add(passwordOutputPane);
+
+        // create password length label
+        JLabel passwordLengthLabel = new JLabel("Password Length:");
+        passwordLengthLabel.setFont(new Font("Dialog", Font.PLAIN, 30));
+        passwordLengthLabel.setBounds(25, 215, 272, 39);
+        add(passwordLengthLabel);
+
+        // create password length input
+        JTextArea passwordLengthInputArea = new JTextArea();
+        passwordLengthInputArea.setFont(new Font("Dialog", Font.PLAIN, 30));
+        passwordLengthInputArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        passwordLengthInputArea.setBounds(310, 215, 192, 39);
+        add(passwordLengthInputArea);
+
+        // create toggle buttons
+        // uppercase letter toggle
+        JToggleButton uppercaseToggle = new JToggleButton("Uppercase");
+        uppercaseToggle.setFont(new Font("Dialog", Font.PLAIN, 30));
+        uppercaseToggle.setBounds(25, 302, 225, 56);
+        add(uppercaseToggle);
+
+        // lowercase letter toggle
+        JToggleButton lowercaseToggle = new JToggleButton("Lowercase");
+        lowercaseToggle.setFont(new Font("Dialog", Font.PLAIN, 30));
+        lowercaseToggle.setBounds(282, 302, 225, 56);
+        add(lowercaseToggle);
+
+        // numbers toggle
+        JToggleButton numbersToggle = new JToggleButton("Numbers");
+        numbersToggle.setFont(new Font("Dialog", Font.PLAIN, 30));
+        numbersToggle.setBounds(25, 373, 225, 56);
+        add(numbersToggle);
+
+        // symbols toggle
+        JToggleButton symbolsToggle = new JToggleButton("Symbols");
+        symbolsToggle.setFont(new Font("Dialog", Font.PLAIN, 30));
+        symbolsToggle.setBounds(282, 373, 225, 56);
+        add(symbolsToggle);
+
+        // create generate button
+        JButton generateButton = new JButton("Generate");
+        generateButton.setFont(new Font("Dialog", Font.BOLD, 32));
+        generateButton.setBounds(155, 477, 222, 41);
+        add(generateButton);
     }
 }
